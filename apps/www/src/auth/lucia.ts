fc31aa0621138535c543d921ea2410df84cfe4af
henri <henri.me@skiff.com>
2024-08-25 10:43:47 +0200

    added lucia auth

---
+++ apps/www/src/auth/lucia.ts
@@ -0,0 +1,22 @@
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { db } from "database";
+import { Lucia, TimeSpan } from "lucia";
+
+const adapter = new PrismaAdapter(db.session, db.user); // eslint-disable-line
+export const lucia = new Lucia(adapter, {
+
+	sessionExpiresIn: new TimeSpan(2, "w"), // 2 weeks
+	sessionCookie: {
+		attributes: {
+			// set to `true` when using HTTPS
+			secure: process.env.NODE_ENV === "production"
+		}
+	}
+});
+
+// IMPORTANT!
+declare module "lucia" {
+	interface Register {
+		Lucia: typeof lucia;
+	}
+}
---
+++ apps/www/src/middleware.ts
@@ -0,0 +1,32 @@
+import { cookies } from "next/headers"
+import { type NextRequest, NextResponse } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+    const origin = request.nextUrl.origin
+
+    // this is just an workaround to handle the auth verification
+    // inside the middleware - Since the middleware is "edge only"
+    // we have to call an internal api endpoint to run the lucia magic ;)
+    const verifyRequest = await fetch(`${origin}/api/auth/verify-session`, {
+        // without this, we can't check the cookie in the called api route
+        headers: { Cookie: cookies().toString() }, // eslint-disable-line 
+    })
+
+    const verifySession = (await verifyRequest.json()) as {
+        valid: boolean
+    }
+
+    if (!verifySession.valid) {
+        // invalid session
+        return NextResponse.redirect(new URL("/auth", request.nextUrl))
+    }
+
+    // everything seems ok
+    return NextResponse.next()
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: '/dashboard/:path*',
+}
