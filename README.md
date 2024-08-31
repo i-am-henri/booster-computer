178aaf391d510ac8c7c497b906c6633e858fff80
henri <henri.me@skiff.com>
2024-08-31 22:53:04 +0200

    added a logo and removed the old readme design

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
-# Booster
+<div align="center">
+   <img src="/brand/logo.svg" alt="booster.computer logo" width="50" height="50" align="right"> 
+   <h1>Booster.computer</h1>
+   <p>The visual boilerplaite generator</p>
+</div>
+<p align="center"> 
+   <a href="/README.md">🏘️ Home</a> 
+   • 
+   <a href="/docs/features.md">🚀 Features</a> 
+   • 
+   <a href="/docs/selfhost.md">💻 Selfhost</a>
+   • 
+   <a href="/docs/contributing.md">🤝 Contributing</a> 
+   • 
+   <a href="/LICENSE">📄 License</a> 
+</p>
+<p align="center"> 
+   <a href="https://github.com/i-am-henri/booster-computer/stargazers"> 
+   <img src="https://img.shields.io/github/stars/i-am-henri/booster-computer" alt="Stars"> </a> 
+   <a href="https://github.com/i-am-henri/booster-computer/network/members"> <img src="https://img.shields.io/github/forks/i-am-henri/booster-computer" alt="Forks"> </a> <a href="https://github.com/i-am-henri/booster-computer/issues"> 
+   <img src="https://img.shields.io/github/issues/i-am-henri/booster-computer" alt="Issues"> 
+   </a> 
+   <a href="https://github.com/i-am-henri/booster-computer/blob/main/LICENSE"> 
+   <img src="https://img.shields.io/github/license/i-am-henri/booster-computer" alt="License"> 
+   </a> 
+</p>
 
-Booster is a tool to quickly create a new project with a graphical user interface. It is based on [Turborepo](https://turborepo.org/) and [Next.js](https://nextjs.org/). Booster allows you to create templates by simply selecting the desired features. After that, you can download the template with our cli and start working on your platform. We are supporting monorepos, multi-package apps, Nextjs apps and more.
+<br />
 
-<image src="/readme/ping.svg" alt="" />We are currently working on a web version of Booster.
+---
+<sub>
+This project is licensed under the <a href="https://github.com/yourusername/booster-computer/blob/main/LICENSE">MIT License</a> and was made by <a href="https://henri.gg">henri</a>
+</sub>
+<iframe style="width:100%;height:auto;min-width:600px;min-height:400px;" src="https://star-history.com/embed?secret=#i-am-henri/booster-computer&Date" frameBorder="0"></iframe>
--- apps/www/src/components/elements/sidebar.tsx
+++ apps/www/src/components/elements/sidebar.tsx
@@ -1,16 +1,16 @@
 "use client"
 import * as RadixDropDownMenu from "@radix-ui/react-dropdown-menu";
-import Avvvatars from 'avvvatars-react';
-import { ChevronsUpDown, Download } from "lucide-react";
+import Avvvatars from "avvvatars-react";
+import { ChevronsUpDown, CircleDollarSign, Download } from "lucide-react";
 import Link from "next/link";
 import { cn } from "node_modules/ui/src/cn";
 import { InputStyles } from "ui";
 
 export default function Sidebar() {
-    const className = "p-2";
+    const className = "px-2 py-1.5 text-sm outline-none transition hover:bg-[#f2f2f2] rounded-md cursor-pointer flex justify-between items-center";
     return (
-        <aside className="fixed top-0 bg-[#f9f9f9] px-3 pt-1.5 left-0 bottom-0 z-50 w-72 border-r border-stone-200 overflow-y-auto space-y-2">
-            <div className="flex flex-col justify-between">
+        <aside className="fixed top-0 bg-[#ffffff] px-3 flex flex-col py-1.5 left-0 bottom-0 z-50 w-72 border-r border-stone-200 overflow-y-auto space-y-2 justify-between">
+            <div className="space-y-3">
                 <RadixDropDownMenu.Root>
                     <RadixDropDownMenu.Trigger className="outline-none border-b flex border-b-neutral-200 group w-full justify-between items-center py-2">
                         <div className="flex space-x-2 items-center">
@@ -21,7 +21,11 @@ export default function Sidebar() {
                         </div>
                         <ChevronsUpDown className="text-secondary h-4 w-4" />
                     </RadixDropDownMenu.Trigger>
-                    <RadixDropDownMenu.Content className="">
+                    <RadixDropDownMenu.Content className="bg-[#ffffff] border border-[#DBDBDB] rounded-lg min-w-64 p-1">
+                        <h2 className="px-2 py-1.5 text-sm font-semibold">
+                            Account
+                        </h2>
+                        <RadixDropDownMenu.Separator />
                         <RadixDropDownMenu.Item className={className}>
                             <a href="/dashboard">Dashboard</a>
                         </RadixDropDownMenu.Item>
@@ -30,8 +34,8 @@ export default function Sidebar() {
                         </RadixDropDownMenu.Item>
                     </RadixDropDownMenu.Content>
                 </RadixDropDownMenu.Root>
+                <input type="text" className={cn(InputStyles, "px-2 py-1")} placeholder="Search" />
                 <div className="flex flex-col">
-                    <input type="text" className={cn(InputStyles, "p-1")} placeholder="Search" />
                     <div className="flex">
                         <p className="text-[#666666]">Platform</p>
                         <hr />
@@ -53,7 +57,6 @@ export default function Sidebar() {
                     </Link>
                 </div>
                 <div className="flex flex-col">
-
                     <Link className="text-lg" href="/dashboard/">
                     </Link>
                     <Link className="text-lg" href="/dashboard/">
@@ -68,9 +71,11 @@ export default function Sidebar() {
                     <Link className="text-lg" href="/dashboard/">
                     </Link>
                 </div>
-                <div className="border-t border-stone-300">
-                    <Link className="flex items-center text-[#222222]" href="https://docs.booster.computer"><Download className="mr-2 h-4 w-4 text-[#222222]" />Docs</Link>
-                </div>
+
+            </div>
+            <div className="border-t border-stone-300 space-y-1 pt-1">
+                <Link className="flex items-center text-[#222222]" href="https://docs.booster.computer"><Download className="mr-2 h-4 w-4 text-[#222222]" />Docs</Link>
+                <Link className="flex items-center text-[#222222]" href="https://docs.booster.computer"><CircleDollarSign className="mr-2 h-4 w-4 text-[#222222]" />Plans</Link>
             </div>
         </aside>
     )
