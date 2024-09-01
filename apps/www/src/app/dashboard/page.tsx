627e14f69c382c82c77485b97bcf21e99db087c2
henri <henri.me@skiff.com>
2024-09-01 17:43:37 +0200

    added some more models

--- apps/www/src/app/dashboard/page.tsx
+++ apps/www/src/app/dashboard/page.tsx
@@ -1,8 +1,10 @@
 import FadeToTop from "@/components/animations/fade-to-top";
+import { db } from "database";
 import Link from "next/link";
 import { Button } from "ui";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+    const templates = await db
     return (
         <FadeToTop className="flex flex-col items-center justify-center min-h-screen w-full">
             <h2 className="text-2xl font-medium">No templates yet created!</h2>
